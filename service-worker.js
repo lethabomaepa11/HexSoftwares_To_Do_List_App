c09c89da5ca782cc1c4a5a15863b698549ae0bee
lethabo <shysnylethabo11@gmail.com>
2024-10-22 23:57:47 +0200

    pwa support

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'todo-app-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/styles.css',
+    '/js/index.js',
+    '/assets/todo.png',
+    // Add any other assets to cache
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Caching files');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event to serve cached files
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response from cache
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate event to clean up old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
